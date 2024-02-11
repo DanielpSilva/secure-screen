b713d127e3915cbca53b44f8626091fdbb286302
Daniel Silva <daniel.jqh@outlook.com>
2024-02-11 09:25:28 -0300

    Adicionando SecurePageRoutes e refatorando nomes

--- backend/src/routes/sessionRoutes.ts
+++ backend/src/routes/sessionRoutes.ts
@@ -1,13 +1,11 @@
 import express from "express";
-import { CreateSessionUseCase } from "../application/useCases/create-session";
-import { SessionController } from "../application/controllers/sessionController";
+import { CreateSessionUseCase } from "../application/useCases/session/create-session";
+import { SessionController } from "../application/controller/session-controller";
 
 const sessionRouter = express.Router();
 const createSessionUseCase = new CreateSessionUseCase();
 const sessionController = new SessionController(createSessionUseCase);
 
-sessionRouter.post("/create-session", (req, res) =>
-  sessionController.createSession(req, res),
-);
+sessionRouter.post("/create-session", (req, res) => sessionController.createSession(req, res));
 
 export { sessionRouter };
