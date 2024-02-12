7a20b726a007b10fb0d0cfb6b262ca1e332f8aa1
Daniel Silva <daniel.jqh@outlook.com>
2024-02-12 13:36:24 -0300

    Corrigindo nomenclatura do usecase Disable para Deactivate

--- backend/src/routes/SecurePageAccessRoutes.ts
+++ backend/src/routes/SecurePageAccessRoutes.ts
@@ -1,12 +1,18 @@
 import express from "express";
-import { SecurePageAccessController } from "../application/controller/secure-page-access-controller";
-import { CheckActiveSecurePageUseCase } from "../application/useCases/secure-page/check-active-secure-page-access";
-import { NewSecurePageAccessUseCase } from "../application/useCases/secure-page/new-secure-page-access";
+import { SecurePageAccessController } from "../application/controller/SecurePageAccessController";
+import { CheckIfExistsActiveSecurePageUseCase } from "../application/useCases/securePage/CheckIfExistsActiveSecurePageAccess";
+import { NewSecurePageAccessUseCase } from "../application/useCases/securePage/NewSecurePageAccess";
+import { DeactivateSecurePageAccessUseCase } from "../application/useCases/securePage/DeactivateSecurePageAccess";
 
 const securePageRouter = express.Router();
-const checkActiveSecurePageUseCase = new CheckActiveSecurePageUseCase();
+const checkActiveSecurePageUseCase = new CheckIfExistsActiveSecurePageUseCase();
 const newSecurePageAccessUseCase = new NewSecurePageAccessUseCase();
-const securePageController = new SecurePageAccessController(checkActiveSecurePageUseCase, newSecurePageAccessUseCase);
+const deactivateSecurePageAccessUseCase = new DeactivateSecurePageAccessUseCase();
+const securePageController = new SecurePageAccessController(
+  checkActiveSecurePageUseCase,
+  newSecurePageAccessUseCase,
+  deactivateSecurePageAccessUseCase,
+);
 
 securePageRouter.post("/check-active-secure-page-acess", (req, res) =>
   securePageController.checkActiveSecurePageAccess(req, res),
