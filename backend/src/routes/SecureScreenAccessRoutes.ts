c8a68976ce830a858d723e3b4b9b1f1eb31738eb
Daniel Silva <daniel.jqh@outlook.com>
2024-02-13 17:39:39 -0300

    Removendo rotas não utlizadas

--- backend/src/routes/SecureScreenAccessRoutes.ts
+++ backend/src/routes/SecureScreenAccessRoutes.ts
@@ -1,25 +1,14 @@
 import express from "express";
 import { SecureScreenAccessController } from "../application/controller/SecureScreenAccessController";
 import { CheckIfExistsActiveSecureScreenUseCase } from "../application/useCases/secureScreen/CheckIfExistsActiveSecureScreenAccess";
-import { NewSecureScreenAccessUseCase } from "../application/useCases/secureScreen/NewSecureScreenAccess";
-import { DeactivateSecureScreenAccessUseCase } from "../application/useCases/secureScreen/DeactivateSecureScreenAccess";
 
 const secureScreenRouter = express.Router();
 const checkActiveSecureScreenUseCase = new CheckIfExistsActiveSecureScreenUseCase();
-const newSecureScreenAccessUseCase = new NewSecureScreenAccessUseCase();
-const deactivateSecureScreenAccessUseCase = new DeactivateSecureScreenAccessUseCase();
-const secureScreenController = new SecureScreenAccessController(
-  checkActiveSecureScreenUseCase,
-  newSecureScreenAccessUseCase,
-  deactivateSecureScreenAccessUseCase,
-);
+
+const secureScreenController = new SecureScreenAccessController(checkActiveSecureScreenUseCase);
 
 secureScreenRouter.post("/check-active-secure-screen-acess", (req, res, next) =>
   secureScreenController.checkActiveSecureScreenAccess(req, res, next),
-);
-
-secureScreenRouter.post("/new-secure-screen-acess", (req, res, next) =>
-  secureScreenController.newSecureScreenAccess(req, res, next),
 );
 
 export { secureScreenRouter };
